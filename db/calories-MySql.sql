1d51450de22a5c3925a7e710c25fc8e9a6eb7dd0
arun2604 <54104684+arun2604@users.noreply.github.com>
2024-12-28 17:47:25 +0530

    Add files via upload

---
+++ db/calories-MySql.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Dec 28, 2024 at 12:02 PM
+-- Server version: 8.0.31
+-- PHP Version: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `calorie_tracker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `calories`
+--
+
+DROP TABLE IF EXISTS `calories`;
+CREATE TABLE IF NOT EXISTS `calories` (
+  `Id` bigint NOT NULL,
+  `FoodName` varchar(255) NOT NULL,
+  `FoodCalorie` bigint NOT NULL,
+  `Date` datetime NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
